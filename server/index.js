83fe057ecaf2c2ada9d5d47cb8108519821ab62c
javier lassus <lassusjavier>
2021-04-16 02:35:43 -0400

    New features Added

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
-const SERVER_PORT = 8080;
+const cors = require("cors");
 const nbcData = require("./data/data.json");
 
 app.use(express.json());
 
+app.use(cors());
+
 // app.use("/data", nbcData);
 
-app.listen(SERVER_PORT, () => {
+app.use("/api/nbcData", nbcData);
+
+app.listen(8080, () => {
   console.log("server is live on port 8080");
 });
