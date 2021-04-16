6d38506a04dbc4a836af6d0dbf47c01c80146107
Javier Lassus <77644743+javierlassus@users.noreply.github.com>
2021-04-15 22:14:48 -0400

    created basic server route

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -1,9 +1,20 @@
 const fs = require("fs");
 const path = require("path");
 
-const projectsData = path.resolve("../data/data.json");
-const projectList = require(projectsData);
+// const projectsData = path.resolve("../data/data.json");
+// const projectList = require(projectsData);
 
 const router = require("express").Router();
+
+router.get("/", (req, res) => {
+  fs.readFile(path.resolve("./data/data.json"), "utf8", (error, data) => {
+    if (error) {
+      console.log(error.message);
+    }
+    console.log(data);
+    const nbcData = JSON.parse(data);
+    res.json(nbcData);
+  });
+});
 
 module.exports = router;
